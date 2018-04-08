53dd729cff3a009d3edc45c2d65d299908087d6a
Alex-Tremayne <alexjtremayne@gmail.com>
2018-04-09 00:01:14 +1000

    Update functions.py

--- reagle/functions.py
+++ reagle/functions.py
@@ -41,7 +41,6 @@ def projectionMatrix(los,printA=False):
 	return array
 
 def velocityCube(obsv, directory, tag, fileType='SNAPSHOT',printA=False):
-	#obsv, the point to observe from as a 3-list
 	position = readArray(fileType, directory, tag, '/PartType0/Coordinates')
 	velocities = readArray(fileType, directory, tag, '/PartType0/Velocity')
 
@@ -59,29 +58,3 @@ def velocityCube(obsv, directory, tag, fileType='SNAPSHOT',printA=False):
 	projection = np.matmul(posShifted,projMat.T)
 
 	return [projection,radialVel,projMat,lineOfSight,centre]
-
-
-
-
-
-
-#los is line of sight vector
-# def projectionMatrix(los):
-# 	if not (np.dot(los,[1,0,0]) in [0,1]): #Make sure LOS vector is not along x,y,z axis
-# 		L1=np.array([1,0,0])
-# 		L2=np.array([0,1,0])
-# 	else:
-# 		L1=np.array([1,1,0])
-# 		L2=np.array([0,1,1])
-# 		print("On axis")
-
-# 	L1=np.subtract(L1,(np.dot(L1,los)*los))
-# 	L1=L1/np.linalg.norm(L1)
-
-	
-# 	L2=np.subtract(L2,(np.dot(L2,L1)*L1))
-# 	L2=np.subtract(L2,(np.dot(L2,los)*los))
-# 	L2=L2/np.linalg.norm(L2)
-	
-# 	array = np.concatenate(([L1], [L2],[[0,0,0]]))
-# 	return array

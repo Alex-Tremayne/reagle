8f16db2fb9aba093448f04fa45405df5bdc418bc
Alex-Tremayne <alexjtremayne@gmail.com>
2018-04-08 14:09:07 +1000

    Add files via upload

---
+++ reagle/setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(name='reagle',
+      version='0.1',
+      description='For creating data visualisations from EAGLE snapshots',
+      url='https://github.com/Alex-Tremayne/reagle',
+      author='Alex Tremayne',
+      author_email='ajtremayne@gmail.com',
+      #license='MIT',
+      packages=['reagle'],
+      zip_safe=False)

5b570e23dbd2ee33601b4ac6244666e1ca218eb7
Alex-Tremayne <alexjtremayne@gmail.com>
2018-04-08 21:58:47 +1000

    Add files via upload

---
+++ setup.py
@@ -0,0 +1,15 @@
+from setuptools import setup
+
+setup(name='reagle',
+      version='0.0.28',
+      description='For creating data visualisations from EAGLE snapshots',
+      url='https://github.com/Alex-Tremayne/reagle',
+      author='Alex Tremayne',
+      author_email='alexjtremayne@gmail.com',
+      license='MIT',
+      classifiers=['Development Status :: 3 - Alpha',
+      'License :: OSI Approved :: MIT License',
+      'Programming Language :: Python :: 3.6'],
+      packages=['reagle'],
+      install_requires=['numpy','h5py'],
+      zip_safe=False)

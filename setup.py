266a22400451cb5af9d19cf8b734dbe5927ff95e
Alex Tremayne <alexjtremayne@gmail.com>
2018-04-18 21:46:50 +1000

    Update setup.py

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 from setuptools import setup
 
 setup(name='reagle',
-      version='0.1.0',
+      version='0.1.6',
       description='For creating data visualisations from EAGLE snapshots',
       url='https://github.com/Alex-Tremayne/reagle',
       author='Alex Tremayne',
